50448d86f0ec5287ddebdb73761072fd200ea030
Schmidt-Aaron <aschmidt808@gmail.com>
2017-04-29 11:29:19 -0700

    minor formatting update

--- app.js
+++ app.js
@@ -2,24 +2,17 @@ var express = require('express');
 var app = express();
 var useragent = require('useragent');
 
-//i need to get ip, language and os of browser
-//main function
+
 app.get('/', function(req, res){
     //do stuff
     var ip = req.ip;
-    var header = JSON.stringify(req.headers);
     var language = req.acceptsLanguages();
     var agent = useragent.parse(req.headers['user-agent']);
-    //console.log(req.headers['host']);
-    //console.log(req.headers["user-agent"]);
-    //console.log(req.acceptsLanguages());
-   // console.log(req.acceptsEncodings);
 
     var output = {
         'ip address': req.headers['host'],
         'language': language[0],
-        'operating system': agent.os.toString()
-        
+        'operating system': agent.os.toString() 
     };
 
     res.send(output);
